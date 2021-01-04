856619c58d2ac42acd0d6e1e85f35a6f90a557fc
Matt <37490308+VE3OY@users.noreply.github.com>
2021-01-03 19:30:05 -0500

    Add files via upload

--- install/conv.sh
+++ install/conv.sh
@@ -1,14 +1,26 @@
 #!/bin/sh
 
 TST1=`ps -A | grep conversd`
-TST2=`ps -A | grep x-bot`
+TST2=`ps -A | grep XBot`
 
 
-if [ "$TST1" = "" ] || [ "$TST2" = ""  ]; then
-    killall conversd
-    killall XBot
+
+p1=$(pidof -s conversd)
+p2=$(pidof -s XBot)
+
+if [ "$p1" = "" ] || [ "$p2" = ""  ]; then
+    echo "CONVERSD or XBot not found running ... starting CONVERSD and XBot"
+    logger "CONVERSD or XBot not found running ... starting CONVERSD and XBot"
+    #
+    # just in case, kill any running process
+    killall conversd > /dev/null 2>&1
+    killall XBot > /dev/null 2>&1
     sleep 1
-    `which conversd`
-    sleep 5
+    /usr/local/sbin/conversd
+    sleep 1
     /usr/local/bin/XBot
+    touch /var/log/conversd.log
+else
+    echo "   CONVERSD is running on PID: `pidof conversd`"
+    echo "   XBot is running on PID: `pidof XBot`"
 fi
